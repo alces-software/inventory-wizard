6f55159d9f961605c5030878b3d8f5bac3f174a2
Ruan G. Ellis <ruan.ellis@alces-software.com>
2019-06-26 12:23:20 +0100

    Updated config script to interactive and functionality to add new clusters.

--- bin/config.rb
+++ bin/config.rb
@@ -8,35 +8,86 @@ $config = 'etc/config.yaml'
 config_hash = YAML.load(File.read($config))
 
 def write_yaml(hash)
-    File.open($config ,"w") do |f|
-      f.write hash.to_yaml
-    end
+  File.open($config ,"w") do |f|
+    f.write hash.to_yaml
   end
+end
 
 def show_clusters(config_hash)
-    sz = config_hash['config']['clusters'].size
-    puts "These clusters are currently configured: "
-    config_hash['config']['clusters'].select do |i|
-      puts "- " +  i['name'] + " in " + i['location']
-    end
+  puts
+  sz = config_hash['config']['clusters'].size
+  puts "These clusters are currently configured: "
+  @cluster_hash = config_hash['config']['clusters'].each_with_index.map do |cluster,index|
+    puts "#{index + 1}) #{cluster['name']} writing to #{cluster['location']}"
+    [index + 1,cluster]
+    #puts "- " +  i['name'] + " in " + i['location']
+  end.to_h
+  puts
 end
 
 def active_cluster(config_hash)
-    cluster_name = config_hash['config']['clusters'].select {|i| i['in_use']}.first['name']
-    location = config_hash['config']['clusters'].select {|i| i['in_use']}.first['location']
-    puts "Active Cluster: " + cluster_name.to_s 
-    puts 
-    #puts "Writing to: " + location.to_s
+  cluster_name = config_hash['config']['clusters'].select {|i| i['in_use']}.first['name']
+  location = config_hash['config']['clusters'].select {|i| i['in_use']}.first['location']
+  puts
+  puts "Active Cluster: " + cluster_name.to_s 
+  puts 
+  #puts "Writing to: " + location.to_s
 end
 
 def switch_active(config_hash)
-    puts "Which cluster would you like to use?"
-    switchto = gets.chomp.to_s
-    config_hash['config']['clusters'].select {|i| i['in_use'] == true }[0]['in_use'] = false
-    config_hash['config']['clusters'].select {|i| i['name'] == switchto }[0]['in_use'] = true
-    write_yaml(config_hash)
+  puts
+  puts "Which cluster would you like to use?"
+  show_clusters(config_hash)
+  print " > "
+  switchto = gets.chomp.to_i
+  @cluster_hash[switchto]
+  config_hash['config']['clusters'].select {|i| i['in_use'] == true }[0]['in_use'] = false
+  @cluster_hash[switchto]['in_use'] = true 
+  write_yaml(config_hash)
+  puts
 end
 
-active_cluster(config_hash)
-show_clusters(config_hash)
-switch_active(config_hash)
+def new_cluster(config_hash)
+  puts
+  puts "New cluster name?"
+  print " > "
+  newname = gets.chomp.to_s
+  puts
+  puts "Path to write to?"
+  print " > "
+  newpath = gets.chomp.to_s
+  newhash = {"name"=>newname, "location"=>newpath, "in_use"=>false}
+  config_hash['config']['clusters'].push(newhash)
+  puts
+  write_yaml(config_hash)
+end
+
+#active_cluster(config_hash)
+#show_clusters(config_hash)
+#switch_active(config_hash)
+
+menu = "Choose Option: 
+   1) Show active cluster
+   2) Show cluster paths
+   3) Change active cluster
+   4) New cluster
+   5) Exit
+
+ > "
+
+loop do
+  print menu
+  opt = gets.chomp.to_i
+  case opt
+  when 1
+    active_cluster(config_hash)
+  when 2
+    show_clusters(config_hash)
+  when 3
+    switch_active(config_hash)
+  when 4
+    new_cluster(config_hash)
+  when 5
+   break
+  end
+end
